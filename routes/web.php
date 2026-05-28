c47c400af0447ba06ca70c6d8915083cc26b4a9c
okti <okti@192.168.1.154>
2026-05-28 18:11:29 +0300

    paketler kuruldu

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->middleware('auth')->group(function () {
+    Route::get('/deneme', [TestController::class, 'test'])->name('test');
+    Route::get('/detail', [TestController::class, 'detail'])->name('detail');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
